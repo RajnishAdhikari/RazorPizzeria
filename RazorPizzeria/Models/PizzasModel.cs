721400b6d2491e642313d404f71d296a16804f29
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-10-17 14:17:22 +0545

    adding some of the properties in CustomPizza

--- RazorPizzeria/Models/PizzasModel.cs
+++ RazorPizzeria/Models/PizzasModel.cs
@@ -2,8 +2,8 @@
 {
     public class PizzasModel
     {
-        public string ImageTitle { get; set; }
-        public string PizzaName { get; set; }
+        public string? ImageTitle { get; set; }
+        public string? PizzaName { get; set; }
         public float BasePrice { get; set; } = 2;
         public bool TomatoSauce { get; set; }
         public bool Cheese { get; set; }
@@ -12,6 +12,7 @@
         public bool Tuna { get; set; }
         public bool Ham { get; set; }
         public bool Chicken { get; set; }
+        public bool Pineapple { get; set; }
         public float FinalPrice { get; set; }
 
     }
--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
+
 
 namespace RazorPizzeria.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel? Pizza { get; set; }
+        public float PizzaPrice { get; set;}
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+
         }
     }
 }
