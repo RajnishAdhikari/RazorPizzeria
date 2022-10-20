f083d387c0f4e3a0df22f00b7e177f6018ba0ff5
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-10-20 20:08:19 +0545

    adding pizza price and name is checkout page  inside checkout which is again inside Pages

--- RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -17,6 +17,19 @@ namespace RazorPizzeria.Pages.Forms
 
         public IActionResult OnPost()
         {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Chicken) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PizzaName, PizzaPrice});
+            
 
         }
     }
